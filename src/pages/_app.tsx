89fd5b5cee770d4ace80b02955262db7396ee349
Julio Azevedo <azevedo.developer@gmail.com>
2024-11-25 08:53:08 -0300

    adding theme configs for entire project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import Head from 'next/head'
 
 import GlobalStyles from '@/styles/global'
 import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import theme from '@/styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-    <Head>
-      <title> React Avançado - Boilerplate</title>
-      <link rel="stylesheet" href="shortcut" />
-    </Head>
-    </>
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title> React Avançado - Boilerplate</title>
+        <link rel="stylesheet" href="shortcut" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,29 +1,52 @@
-'use client'
-
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap');
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: local('Poppins Light'), local('Poppins-Light'),
+        url('/fonts/poppins-v12-latin-300.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local('Poppins Regular'), local('Poppins-Regular'),
+        url('/fonts/poppins-v12-latin-regular.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+        url('/fonts/poppins-v12-latin-600.woff2') format('woff2');
+  }
 
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
-    -moz-osx-smoothing: grayscale;
+    -moz-osx-font-smoothing: grayscale;
   }
 
-  html {
-    font-size: 62.5%; // 1rem = 10px == 10/16px = 62.5% (1.4rem = 14px)
-  }
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
 
-  html, body, #__next {
-    height: 100%;
-  }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
 
-  body {
-    font-family: "Poppins", -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
 `
 
 export default GlobalStyles
