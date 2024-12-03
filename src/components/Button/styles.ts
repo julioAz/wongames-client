1f07d6688f3e954f06bbc8252742b2656d9f1473
Julio Azevedo <azevedo.developer@gmail.com>
2024-12-03 08:50:24 -0300

    adding button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,13 @@
+import styled, { css } from 'styled-components'
+import { ButtonProps } from '.'
+
+type WrapperProps = Pick<ButtonProps, 'size'>
+
+export const Wrapper = styled.button<WrapperProps>`
+  ${({ theme, size }) => css`
+    background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
+    color: ${theme.colors.white};
+    border: none;
+    border-radius: ${theme.border.radius};
+  `}
+`
