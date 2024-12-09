72823a15b9f64c8d440fca595e0d410cffd14818
Julio Azevedo <azevedo.developer@gmail.com>
2024-12-09 14:54:40 -0300

    adding tests for button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,7 +1,24 @@
-import styled, { css } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 import { ButtonProps } from '.'
 
 type WrapperProps = Pick<ButtonProps, 'size'>
+
+const warapperModifiers = {
+  small: (theme: DefaultTheme) => css`
+    height: 3rem;
+    font-size: ${theme.font.sizes.xsmall};
+  `,
+  medium: (theme: DefaultTheme) => css`
+    height: 4rem;
+    font-size: ${theme.font.sizes.small};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
+  `,
+  large: (theme: DefaultTheme) => css`
+    height: 5rem;
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
+  `
+}
 
 export const Wrapper = styled.button<WrapperProps>`
   ${({ theme, size }) => css`
@@ -9,5 +26,8 @@ export const Wrapper = styled.button<WrapperProps>`
     color: ${theme.colors.white};
     border: none;
     border-radius: ${theme.border.radius};
+    padding: ${theme.spacings.xxsmall};
+
+    ${!!size && warapperModifiers[size](theme)};
   `}
 `
--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,13 +1,32 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Button from '.'
+import { renderWithTheme } from '@/utils/tests/helpers'
 
 describe('<Button />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Button />)
+  it('should render a medium size button by default', () => {
+    renderWithTheme(<Button>Buy now</Button>)
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
+  })
 
-    expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument()
+  it('should render a small size button by default', () => {
+    renderWithTheme(<Button size="small">Buy now</Button>)
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '3rem',
+      'font-size': '1.2rem'
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a medium size button by default', () => {
+    renderWithTheme(<Button size="large">Buy now</Button>)
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      height: '5rem',
+      padding: '0.8rem 4.8rem',
+      'font-size': '1.6rem'
+    })
   })
 })
